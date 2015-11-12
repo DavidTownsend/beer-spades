e1f0eafd03839bb78b8c56f5e9735cde03d56270
David Townsend <e393jz@erac.com>
2015-11-11 22:04:41 -0600

    moving methods

--- app/src/main/java/com/lol/beer_spades/render/BaseRenderActivity.java
+++ app/src/main/java/com/lol/beer_spades/render/BaseRenderActivity.java
@@ -1,6 +1,9 @@
 package com.lol.beer_spades.render;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,7 +47,7 @@ public abstract class BaseRenderActivity extends Activity{
     }
 
     protected void setupImageViewFromCard(Card card, ImageView imageView, Integer x_position, Integer y_position){
-        imageView.setImageBitmap(CardUtilities.decodeSampledBitmapFromResource(getResources(), card.getResourceId(), ScreenUtilities.getCardHeight(display), ScreenUtilities.getCardHeight(display)));
+        imageView.setImageBitmap(decodeSampledBitmapFromResource(getResources(), card.getResourceId(), ScreenUtilities.getCardHeight(display), ScreenUtilities.getCardHeight(display)));
         imageView.setMaxHeight(ScreenUtilities.getCardHeight(display));
         imageView.setAdjustViewBounds(true);
         imageView.setId(card.getId());
@@ -88,5 +91,44 @@ public abstract class BaseRenderActivity extends Activity{
         selectedCard.setSelected(true);
         imageView.setY(imageView.getY() - ScreenUtilities.getSelectedCardYIncrease(display));
         relativeLayout.setClickable(true);
+    }
+
+    protected Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    private int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
--- app/src/main/java/com/lol/beer_spades/utils/CardUtilities.java
+++ app/src/main/java/com/lol/beer_spades/utils/CardUtilities.java
@@ -1,11 +1,5 @@
 package com.lol.beer_spades.utils;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-
 import com.lol.beer_spades.game.Card;
 import com.lol.beer_spades.game.SuitType;
 import com.lol.beer_spades.player.Player;
@@ -43,23 +37,6 @@ public class CardUtilities {
         suit.add(SuitType.diamonds);
 
         return suit;
-    }
-
-    // TODO not being used
-    public static void setupBasicImageProperties(ImageView imageView, Card card, Integer x_position, Integer y_position){
-        imageView.setImageResource(card.getResourceId());
-        imageView.setMaxHeight(165);
-        imageView.setMaxWidth(165);
-        imageView.setAdjustViewBounds(true);
-        imageView.setId(card.getId());
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-        if(null != x_position && null != y_position) {
-            lp.topMargin = y_position;
-            lp.leftMargin = x_position;
-        }
-
-        imageView.setLayoutParams(lp);
     }
 
     public static Card getCard(List<Card> allCards, int id){
@@ -108,44 +85,5 @@ public class CardUtilities {
         Collections.sort(player2.getCards());
         Collections.sort(player3.getCards());
         Collections.sort(player4.getCards());
-    }
-
-    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
-
-    private static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
     }
 }
